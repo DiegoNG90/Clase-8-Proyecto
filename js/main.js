8ae172ed3efc21928bfe9cc9559d616fada10b12
Diego <diego.nicolas.gandara@gmail.com>
2021-04-01 17:06:36 -0300

    Se limpia main.js, sacando los antiguos efectos sobre proyectos.html y también se limpian la background-image sobre el body en CSS

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ console.log("linkeado 100%");
 const $inicio = document.querySelector('#inicio');
 $inicio.onclick = function (){
     $inicio.style.color = '#5f0f40';
-    $inicio.style.fontWidth = "bolder"     
+    $inicio.style.fontWeight = "bolder"     
 }
 //Se remueven efectos sobre SKILLS
 // const $skills = document.querySelector('#skills')
@@ -21,66 +21,12 @@ $contacto.onclick = function (){
 
 const $brand = document.querySelector('.navbar-brand');
 $brand.onclick = function(){
-    $brand.style.webkitAnimation = 'rotate-out-center 0.8s ease-out alternate-reverse backwards;';
+    $brand.style.webkitAnimation = 'rotate-out-center 0.8s ease-out alternate-reverse backwards';
 }
 
 
-// Pagina Proyectos
-const $footerLight = document.querySelector('.footer-proyecto');
-const $bodyProyectos = document.querySelector('.proyecto-body');
-const $sectionCarrusel = document.querySelector('.carrusel');
-const $proyectosOcultos = document.querySelector('.proyectos-ocultos');
-const $navOscuro = document.querySelector('.header-proyectos');
-const $carrusel = document.querySelector('#carouselExampleSlidesOnly');
-const $titulo = document.querySelector('#oscuridad h1');
-const $mundoGeek = document.querySelector('#mundoGeek');
-const $petFirmware = document.querySelector('#pet-firmware');
-
-//Evento prender la luz
-$footerLight.onclick = function(){
-    $footerLight.style.backgroundColor = "white";
-    $footerLight.children[0].style.backgroundColor = "White";
-    $footerLight.children[0].style.color = "black";
-    $footerLight.style.animation = "heartbeat 0s ease-in-out infinite both";
-    //Pensar mejor una transicion con un background más copado.
-    $bodyProyectos.style.backgroundColor = "white";
-    $titulo.style.color ="black";
-    $titulo.textContent = "Yo!";
-    $titulo.style.margin = "0.2rem"
-
-
-    // $mundoGeek.classList.remove("oculto");
-    $sectionCarrusel.style.backgroundColor = "white";
-    //Oculto el carrusel
-    $carrusel.style.display = "none";
-    $proyectosOcultos.style.display = "flex";
-    // $navOscuro.style.backgroundColor = "White";
-    $navOscuro.style.opacity = "1";
-    $navOscuro.style.display = "flex";
-    $navOscuro.className = "header-proyectos sticky-top";
-}
-
-
-//Doble evento para mostrar las características del proyecto
-//Evento hover
-$mundoGeek.addEventListener('mouseover', function(){
-    $mundoGeek.children[0].classList.remove("oculto")
-})
-
-$mundoGeek.addEventListener('mouseout', function(){
-    $mundoGeek.children[0].classList.add("oculto")
-})
-
-$petFirmware.addEventListener('mouseover', function(){
-    $petFirmware.children[0].classList.remove("oculto")
-})
-
-$petFirmware.addEventListener('mouseout', function(){
-    $petFirmware.children[0].classList.add("oculto")
-})
 
 // PROYECTOS3.HTML : Efecto hover sobre la imagen que afecta al texto de las cards
-
 const cardImages = document.querySelectorAll(".card img");
 const cardText = document.querySelectorAll(".card-text");
 
